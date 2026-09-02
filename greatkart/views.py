60ce0c744ef97d6f3b0b93f39c6c00ef1fa18606
R.DurgaDevi <zoyadeveloper1@gmail.com>
2026-09-02 12:08:58 +0530

    Update project

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,32 +1,46 @@
 # store/views.py
+
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 
-# Home page - all available products
+# Home page
 def home(request):
-    Products = Product.objects.all().filter(is_available=True)  # correct spelling
-    context ={
-         'product':Products,
-     }
+    # Get all available products
+    products = Product.objects.filter(is_available=True)
 
-    return render(request, 'home.html', {'Products': Products})
+    # Get all categories
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'home.html', context)
+
 
 # Category pages
 def electronics(request):
     return render(request, 'categories/electronics.html')
 
+
 def fashion(request):
     return render(request, 'categories/fashion.html')
+
 
 def grocery(request):
     return render(request, 'categories/grocery.html')
 
+
 def books(request):
     return render(request, 'categories/books.html')
 
+
 def toys(request):
     return render(request, 'categories/toys.html')
+
 
 def sports(request):
     return render(request, 'categories/sports.html')
